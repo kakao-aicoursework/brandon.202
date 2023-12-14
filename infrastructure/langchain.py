5a52e0935fc368fdbf4a0862a0b97caf027f2de8
kok202 <kok202@naver.com>
2023-12-14 16:04:49 +0900

    카카오톡 관련 질문 응답 확인

--- infrastructure/langchain.py
+++ infrastructure/langchain.py
@@ -1,48 +1,67 @@
 from dotenv import load_dotenv
 from langchain.chains import LLMChain
-from langchain.chains import SequentialChain
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts.chat import ChatPromptTemplate
+from infrastructure.vectorstore import query_on_chroma
 
 # load .env
 load_dotenv()
 
-# ChatGPT
-llm = ChatOpenAI(
-  temperature=0,
-  max_tokens=3000,
-  model="gpt-3.5-turbo-16k")
 
-
-def read_template_text(file_path):
+def read_file(file_path):
   with open(file_path, 'r', encoding='utf-8') as file:
     total_text = file.read()
   return total_text
 
 
-def create_template_chain(template_path, output_key):
+def create_template_chain(llm, template_path, output_key):
   return LLMChain(
     llm=llm,
     prompt=ChatPromptTemplate.from_template(
-      template=read_template_text(template_path),
+      template=read_file(template_path),
     ),
     output_key=output_key,
     verbose=True,
   )
 
 
-def generate_message(user_message) -> dict[str, str]:
-  preprocess_chain = SequentialChain(
-    chains=[
-      create_template_chain("./infrastructure/templates/retrieve_template.txt", "response"),
-    ],
-    input_variables=["user_message"],
-    output_variables=["response"],
-    verbose=True,
-  )
+def create_chains():
+  llm = ChatOpenAI(
+    temperature=0,
+    max_tokens=3000,
+    model="gpt-3.5-turbo-16k")
+  return {
+    'guess_intent': create_template_chain(
+      llm=llm,
+      template_path="./infrastructure/templates/guess_intent.txt",
+      output_key="intent",
+    ),
+    'retrieve_kakao_data': create_template_chain(
+      llm=llm,
+      template_path="./infrastructure/templates/retrieve_template.txt",
+      output_key="output",
+    ),
+    'default': create_template_chain(
+      llm=llm,
+      template_path="./infrastructure/templates/default_response_template.txt",
+      output_key="output",
+    )
+  }
 
-  context = preprocess_chain(dict(
-    user_message=user_message
-  ))
-  print(context)
-  return context["response"]
+
+def generate_message(user_message) -> dict[str, str]:
+  chains = create_chains()
+  context = dict(user_message=user_message)
+  context["input"] = context["user_message"]
+  context["intent_list"] = read_file(
+    "./infrastructure/templates/intent_list.txt")
+  intent = chains["guess_intent"].run(context)
+
+  print(intent)
+  if intent == "retrieve_kakao_data":
+    context["retrieve_result"] = query_on_chroma(context["user_message"])
+    result = chains["retrieve_kakao_data"].run(context)
+  else:
+    result = chains["default"].run(context["user_message"])
+  print(result)
+  return result
---
+++ infrastructure/vectorstore.py
@@ -0,0 +1,66 @@
+import os
+
+from dotenv import load_dotenv
+from langchain.document_loaders import (
+  TextLoader,
+)
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.vectorstores import Chroma
+
+# load .env
+load_dotenv()
+
+CHROMA_COLLECTION_NAME = "kakao-bot"
+CHROMA_PERSIST_PATH = "./chroma/kakao-persist"
+
+def upload_embedding_from_file(file_path):
+  loader = {
+    "txt": TextLoader,
+  }.get(file_path.split(".")[-1])
+  if loader is None:
+    raise ValueError("Not supported file type")
+  documents = loader(file_path).load()
+  text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=100)
+  docs = text_splitter.split_documents(documents)
+  print(docs, end='\n\n\n')
+
+  Chroma.from_documents(
+    docs,
+    OpenAIEmbeddings(),
+    collection_name=CHROMA_COLLECTION_NAME,
+    persist_directory=CHROMA_PERSIST_PATH,
+  )
+  print('db success')
+
+
+def upload_embeddings_from_dir(dir_path):
+  failed_upload_files = []
+  for root, dirs, files in os.walk(dir_path):
+    for file in files:
+      if file.endswith(".txt"):
+        file_path = os.path.join(root, file)
+
+        try:
+          upload_embedding_from_file(file_path)
+          print("SUCCESS: ", file_path)
+        except Exception as e:
+          print("FAILED: ", file_path + f"by({e})")
+          failed_upload_files.append(file_path)
+
+
+# upload_embeddings_from_dir("./infrastructure/data")
+db = Chroma(
+  persist_directory=CHROMA_PERSIST_PATH,
+  collection_name=CHROMA_COLLECTION_NAME,
+  embedding_function=OpenAIEmbeddings(),
+)
+retriever = db.as_retriever()
+
+def query_on_chroma(query: str, use_retriever: bool = False) -> list[str]:
+  if use_retriever:
+    docs = retriever.get_relevant_documents(query)
+  else:
+    docs = db.similarity_search(query)
+  str_docs = [doc.page_content for doc in docs]
+  return str_docs
